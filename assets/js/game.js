e756e71bdd4fb1be45cab7d8e6df935c2d8c32c4
bklynate <nathanielcfa@gmail.com>
2016-11-26 17:18:24 -0500

    Add new UI game elements || working on game logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,32 @@
 var $ken = $('.ken');
+var $playerSelectionArea = $(".player-selection-area");
 
 /* javascript */
-$(document).on('keydown', function(e) { // 'e' stands for event
-  if (e.key === 'p') {
-    console.log("This is listening");
-    $ken.addClass('punch');
-    setTimeout(function() { $ken.removeClass('punch'); }, 150);
-  }
 
-  if (e.key === 'k') {
-    console.log("This is listening");
-    $ken.addClass('kick');
-    setTimeout(function() { $ken.removeClass('kick'); }, 500);
-  }
+$(document).ready(function() {
 
-  if (e.key === 's') {
-    console.log("This is listening");
-    $ken.addClass('shoryuken');
-    setTimeout(function() { $ken.addClass('down'); }, 500);
-    setTimeout(function() { $ken.removeClass('shoryuken down'); }, 1000);
-  }
+  $("#ken_img").on('click', function(){
+    $("#ken_p").fadeToggle(750);
+  })
+
+  $(document).on('keydown', function(e) { // 'e' stands for event
+    if (e.key === 'p') {
+      console.log("This is listening");
+      $ken.addClass('punch');
+      setTimeout(function() { $ken.removeClass('punch'); }, 150);
+    }
+
+    if (e.key === 'k') {
+      console.log("This is listening");
+      $ken.addClass('kick');
+      setTimeout(function() { $ken.removeClass('kick'); }, 500);
+    }
+
+    if (e.key === 's') {
+      console.log("This is listening");
+      $ken.addClass('shoryuken');
+      setTimeout(function() { $ken.addClass('down'); }, 500);
+      setTimeout(function() { $ken.removeClass('shoryuken down'); }, 1000);
+    }
+  });
 });
