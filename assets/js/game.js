8769630af6b786ad44e21b66b1f038f60c946e6a
bklynate <nathanielcfa@gmail.com>
2016-12-02 21:58:35 -0500

    working on game logic

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,6 @@
+var $kenImg = $("#ken_img");
+var $ryuImg = $("#ryu_img");
+
 var game = {
   // object of all the characters in the game
   characters: {
@@ -21,23 +24,41 @@ var game = {
   computerPlayer: "",
   computerHp: 0,
   computerAttack: 0,
-  gamePlayers: [],
-  opponentsLeft: true,
-  // variables to manipulate the DOM
-  playerElement: $('#attacker'),
-  defenderElement: $('#defender'),
 
   // game functions
   assignPlayers: function(player) {
     if(this.player == '') {
       this.player = player;
-      console.log(player);
+      console.log(this.player);
+      this.playerHp = this.characters[this.player].hp;
+      this.playerAttack = this.characters[this.player].atk;
+      console.log('player attack: ', this.playerAttack);
+      console.log('player health: ', this.playerHp);
+      $(".attacker").append($("<div class='" + this.player + " " + this.player + "_stance'>"));
+      for(var prop in this.characters){
+        if(prop !== this.player){
+          this.computerPlayer = prop;
+        }
+      }
+      $(".flipped").append($("<div class='" + this.computerPlayer + " " + this.computerPlayer + "_stance'>"));
+      this.computerHp = this.characters[this.computerPlayer].hp;
+      this.computerAttack = this.characters[this.computerPlayer].atk;
     }
+    
+  },
+  startGame: function() {
+
   }
 }
 
 $(document).ready(function(){
-  $("img").on('click', function(e){
-    console.log(e.currentTarget.offsetParent.id);
-  })
-})
+  $kenImg.on('click', function(){
+    var data = $(this).data("name")
+    game.assignPlayers(data);
+  });
+
+  $ryuImg.on('click', function(){
+    var data = $(this).data("name")
+    game.assignPlayers(data);
+  });
+});
